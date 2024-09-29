a52b79c1d66496c86ec98e5da2e9cdf0d9643510
MinThetNaung123 <u6530142@au.edu>
2024-09-29 20:29:14 +0700

    update:mongodb

--- app/api/task/route.js
+++ app/api/task/route.js
@@ -1,13 +1,3 @@
-// import Task from '@/models/task';
-// import {connectMongoDB} from '@/libs/mongodb';
-// import { NextResponse } from 'next/server';
-
-// export async function POST(request) {
-//     const {title, description} = await request.json();
-//     await connectMongoDB();
-//     await Task.create({title, description});
-//     return NextResponse.json({message: "Task created"}, {status: 201});
-// }
 
 import Task from '@/models/task';
 import connectMongoDB from '@/libs/mongodb';
@@ -27,3 +17,30 @@ export async function POST(request) {
     return NextResponse.json({ message: 'Error creating task', error: error.message }, { status: 500 });
   }
 }
+
+// export async function GET(request) {
+//     try {
+//       // Connect to MongoDB
+//       await connectMongoDB();
+//       const tasks = await Task.find();
+//       return NextResponse.json({ tasks });
+//     } catch (error) {
+//       console.error('Error fetching tasks:', error);
+//       return NextResponse.json({ message: 'Error fetching tasks', error: error.message }, { status: 500 });
+//     }
+// }
+
+// export async function DELETE(request) {
+//     try {
+//       // Parse the request body
+//       const { id } = await request.json();
+
+//       // Connect to MongoDB
+//       await connectMongoDB();
+//       const deletedTask = await Task.findByIdAndDelete(id);
+//       return NextResponse.json({ message: 'Task deleted successfully', task: deletedTask });
+//     } catch (error) {
+//       console.error('Error deleting task:', error);
+//       return NextResponse.json({ message: 'Error deleting task', error: error.message }, { status: 500 });
+//     }
+// }
--- libs/mongodb.js
+++ libs/mongodb.js
@@ -1,9 +1,9 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const connectMongoDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI);
-        console.log("MongoDB connected"); 
+        console.log("MongoDB connected");
     } catch (error) {
         console.log(error);
     }
